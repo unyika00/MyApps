f3073743102e54b78e1585491b7de19afcc282fd
unyika00 <unyiandika@freemail.hu>
2019-03-06 13:05:46 +0100

    #3: alap switch kezelése

---
+++ getKey/getKey/Program.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+               
+                switch (Console.ReadKey(true).KeyChar)
+                {
+
+                    case '0':
+                        Console.WriteLine("szám");
+                        break;
+                    case '1':
+                        Console.WriteLine("szám");
+                        break;
+                    case '2':
+                        Console.WriteLine("szám");
+                        break;
+                    case '3':
+                        Console.WriteLine("szám");
+                        break;
+                    case '4':
+                        Console.WriteLine("szám");
+                        break;
+                    case '5':
+                        Console.WriteLine("szám");
+                        break;
+                    case '6':
+                        Console.WriteLine("szám");
+                        break;
+                    case '7':
+                        Console.WriteLine("szám");
+                        break;
+                    case '8':
+                        Console.WriteLine("szám");
+                        break;
+                    case '9':
+                        Console.WriteLine("szám");
+                        break;
+                    default:
+                        Console.WriteLine("betû");
+                        break;
+                    case 'q':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'w':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'e':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'r':
+                        Console.WriteLine("betû");
+                        break;
+                    case 't':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'z':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'u':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'i':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'o':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'p':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'õ':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'ú':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'a':
+                        Console.WriteLine("betû");
+                        break;
+                    case 's':
+                        Console.WriteLine("betû");
+                        break;
+                    case 'd':
+                        Console.WriteLine("betû");
+                        break;
+
+
+
+
+
+
+                }
+
+
+            } while (true);
+
+           
+
+            
+        }
+    }
+}
