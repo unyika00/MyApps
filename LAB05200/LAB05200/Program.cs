7a5acfb86d2f224feb6bf256342cb8f464d5ebf6
unyika00 <unyiandika@freemail.hu>
2019-05-22 11:04:57 +0200

    5525258

--- LAB05200/LAB05200/Program.cs
+++ LAB05200/LAB05200/Program.cs
@@ -52,18 +52,59 @@ namespace LAB05200
 
             //Feladat 50 ig a primszámok keresése
             //primszám egyel és önmagán kivül nem lehet osztani 
-            for(int i = 0; i < 50; i++)
+            for (int i = 0; i < 50; i++)
             {
                 if ((i % 3) == 0)
                 {
-                    Console.WriteLine(i);
+                    int[] szamok = new int[30];
+                    Random rnd = new Random();
+                    int min = 1, max = 100,  j, k, m, n;
+                    bool b = true;
+                    for (i = 0; i < 30; i++)
+                    {
+                        do
+                        {
+                            j = i - 1;
+                            k = rnd.Next(min, max);
+                            while (j > -1)
+                            {
+                                if (k == szamok[j])
+                                {
+                                    k = 0;
+                                    j = 0;
+                                }
+                                j--;
+                            }
+                            if (k > 0)
+                            {
+                                szamok[i] = k;
+                                b = true;
+                                if (k != 2)
+                                {
+                                    if (k < 2 || (k % 2) == 0) b = false;
+                                    else
+                                    {
+                                        m = 3;
+                                        n = (int)Math.Sqrt(k);
+                                        while (m <= n && b)
+                                        {
+                                            if ((k % m) == 0) b = false;
+                                            m += 2;
+                                        }
+                                    }
+                                }
+                            }
+                        } while (k == 0);
+                        Console.WriteLine(k + ":    " + (b ? "Prímszám." : "Nem Prímszám."));
+                    }
+                    Console.ReadLine();
 
                 }
 
 
                 Console.ReadKey();
 
-            
+            }
         }
     }
 }
